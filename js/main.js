fdec17914099317a2187d094c5a1c8456a5f663d
ilovevladskoch <skochipec@yandex.ru>
2022-08-30 18:43:04 +0300

    ver 2.1

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@ const mobileNav = document.querySelector('.mobile__nav');
 
 navBtn.addEventListener('click', function () {
     mobileNav.classList.toggle('mobile__nav__active');
+    navBtn.classList.toggle('nav__mobile__close');
 })
